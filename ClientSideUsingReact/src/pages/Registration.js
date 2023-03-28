d8d0e3cf3e6f662ecee363527f84e8b318644bde
Sukhendu Mandal <76108642+sukhendu02@users.noreply.github.com>
2023-03-28 13:56:01 +0530

    Update Registration.js

--- ClientSideUsingReact/src/pages/Registration.js
+++ ClientSideUsingReact/src/pages/Registration.js
@@ -44,32 +44,35 @@ export default function Registration() {
 
   return (
     <>
-      <div className="register_container container  h-[90vh]  ">
-        <div className="row p-2  ">
+    <div className='form-container' >
+      <div className="register_container container  ">
+        <div className="row p-2 d-flex justify-content-center ">
 
-          <div className="col-md-5 mt-16 lg:mt-0 hidden md:block">
-            <img src={RegistrationImg} className="img-fluid w-100 h-auto" alt="" />
+          <div className="col-md-5 d-flex  lg:mt-0 hidden md:block">
+            <img src={RegistrationImg} className="img-fluid w-90 h-auto" alt="" />
           </div>
 
-          <div className="col-md-7 pt-6 flex flex-col justify-center form_container mt-14 sm:mt-0">
+          <div className="col-md-6 pt-6 flex flex-col justify-center form_container mt-14 sm:mt-0">
             <div className="forFormOuterBorder">
-              <form method='POST' className='form-group formContent flex flex-col gap-2 shadow-lg'>
+              <form method='POST' className='form-group formContent flex flex-col gap-2 '>
 
-                <CgProfile style={{color:'007dff',fontSize:'80px'}}/>
-                <h1 className='text-lg sm:text-xl'>Create your account</h1>
+                {/* <CgProfile style={{color:'007dff',fontSize:'80px'}}/> */}
+                <p className='Sign-up-page-heading text-center'>Sign Up</p>
+                <p className='text-secondary m-2'>Create a free accout with your email.</p>
                 <input type="text" className="form-control form_input" id="InputName" name='name' value={userSignup.name} onChange={handleUserSignupInput} placeholder="Enter your name"/>
                 <input type="email" className="form-control form_input" id="InputEmail" name='email' value={userSignup.email} onChange={handleUserSignupInput}  aria-describedby="emailHelp" placeholder="Enter your email"/>         
                 <input type="number" className="form-control form_input" id="InputNumber" name='mobile' value={userSignup.mobile} onChange={handleUserSignupInput}  placeholder="Mobile number"/>
                 <input type="password" className="form-control form_input" id="InputPassword" name='password' value={userSignup.password} onChange={handleUserSignupInput}  placeholder="Password"/>
  
-                <button type="submit" className="btn btn-primary" onClick={sendDataToServer}>SignUp</button>
-                <Link to='/login' className='formAnchor'>Already have an account | SignIn</Link>
+                <button type="submit" className="btn btn-primary" onClick={sendDataToServer}>Sign Up</button>
+                <Link to='/login'  className='signin-link my-4'>Already have an account? Sign In</Link>
                 
               </form>
             </div>
           </div>
 
         </div>
+      </div>
       </div>
     </>
   )
